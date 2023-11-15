70676f706f2f0f58b896249d1fa12f2e06b862e3
Khang Huu Nguyen <nhk2005.personal@gmail.com>
2023-11-15 10:31:04 -0500

    Finish UI for edit function

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faPen} from "@fortawesome/free-solid-svg-icons";
 
 export default function Task({id, title, onDelete}) {
+  // Two Modes: edit + view
+  const [mode, setMode] = useState("edit")
+
   return (
     <li>
       <div>
-        <p>{title}</p>
+        {mode === "view" && <p>{title}</p>}
+        {mode === "edit" && <input value={title} />}
         <div className="icon-container">
           <FontAwesomeIcon icon={faCircleXmark} onClick={() => onDelete(id)}/>
           <FontAwesomeIcon icon={faPen} />
