7a70adaaed2cc0f3f1e69c6a5fbc7091182b9504
Khang Huu Nguyen <nhk2005.personal@gmail.com>
2023-11-16 08:41:30 -0500

    Limit simultaneous edits to one at a time

--- src/Task.jsx
+++ src/Task.jsx
@@ -2,25 +2,23 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faPen, faCheck} from "@fortawesome/free-solid-svg-icons";
 
-export default function Task({task, onDelete, onEdit}) {
-  // Two Modes: edit + view
-  const [mode, setMode] = useState("view");
+export default function Task({task, onDelete, onEdit, isBeingEdited, setEditID}) {
   const [title, setTitle] = useState(task.title);
 
   return (
     <li>
       <div>
-        {mode === "view" && <p>{title}</p>}
-        {mode === "edit" && <input className="edit-bar" value={title} onChange={(e) => {
+        {!isBeingEdited && <p>{title}</p>}
+        {isBeingEdited && <input className="edit-bar" value={title} onChange={(e) => {
           setTitle(e.target.value);
         }} />}
         <div className="icon-container">
           <FontAwesomeIcon icon={faCircleXmark} onClick={() => onDelete(task.id)}/>
-          {mode === "view" && <FontAwesomeIcon icon={faPen} onClick={() => {
-            setMode("edit");
+          {!isBeingEdited && <FontAwesomeIcon icon={faPen} onClick={() => {
+            setEditID(task.id);
           }} />}
-          {mode === "edit" && <FontAwesomeIcon icon={faCheck} onClick={() => {
-            setMode("view");
+          {isBeingEdited && <FontAwesomeIcon icon={faCheck} onClick={() => {
+            setEditID(null);
             onEdit(task.id, document.querySelector(".edit-bar").value);
           }} />}
         </div>
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Task from "./Task";
 
 export default function TaskList({ taskList, onDelete, onEdit }) {
+  const [editID, setEditID] = useState(null);
+
   return (
     <ul>
       {taskList.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} onEdit={onEdit} />
+        <Task key={task.id} task={task} onDelete={onDelete} onEdit={onEdit} isBeingEdited={editID === task.id} setEditID={setEditID} />
       ))}
     </ul>
   );
